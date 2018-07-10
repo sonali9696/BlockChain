5190709f405b1fb726ff2e499600671530bbe6aa
Sonali Agrawal <soagraw@microsoft.com>
2018-07-10 12:53:16 +0530

    Javascript block chain

---
+++ BlockChain/js/main.js
@@ -0,0 +1,15 @@
+﻿// Your code here!
+class Block {
+    constructor(index, timestamp, data, previousHash = '') //index=pos of block, timestamp=time of creation
+    {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = '';
+    }
+
+    calculateHash() {
+
+    }
+}
